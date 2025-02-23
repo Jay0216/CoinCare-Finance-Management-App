1ff226c8742a2114c2266b1716e785490f8e16af
navod <navoddulanjana14@gmail.com>
2025-02-23 12:10:27 +0530

    Changed name little bit of Greetings Function

--- CoinCareApp/app/src/main/java/com/example/coincareapp/MainActivity.kt
+++ CoinCareApp/app/src/main/java/com/example/coincareapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CoinCareAppTheme {
-        Greeting("Navod Jay")
+        Greeting("Navod Jayasundara")
     }
 }
